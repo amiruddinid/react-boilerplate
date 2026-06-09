f2c3dcd447336cea036fa7a9c6b133c792f62e5b
Amiruddin Irsyad <amier.dev@gmail.com>
2026-06-09 11:48:47 +0700

    material create [wip]

--- src/features/master/material/api/create-material.ts
+++ src/features/master/material/api/create-material.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { api } from '@/lib/api-client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { MutationConfig } from '@/lib/react-query';
+import { getMaterialQueryOptions } from './get-material';
 
 export const createMaterialInputSchema = z.object({
     PART_NUMBER: z.string().min(1, 'Part number is required'), // validasi part number harus string dan tidak kosong
@@ -12,3 +13,30 @@ export const createMaterialInputSchema = z.object({
 });
 
 export type CreateMaterialInput = z.infer<typeof createMaterialInputSchema>;
+
+export const createMaterial = async (data: CreateMaterialInput) => {
+    const response = await api.post('/master/material', data);
+    return response.data;
+};
+
+export type UseCreateMaterialOption = {
+    mutationConfig?: MutationConfig<typeof createMaterial>
+} 
+
+export const useCreateMaterial = ({
+    mutationConfig,
+}: UseCreateMaterialOption = {}) => {
+    const queryClient = useQueryClient();
+    const { onSuccess, ...restConfig } = mutationConfig || {}
+
+    return useMutation({
+        onSuccess: (...args) => {
+            queryClient.invalidateQueries({
+                queryKey: getMaterialQueryOptions().queryKey,
+            }),
+            onSuccess?.(...args)
+        },
+        ...restConfig,
+        mutationFn: createMaterial
+    })
+}
